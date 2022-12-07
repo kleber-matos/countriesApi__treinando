2183864a0320d3344aa70bfbe2f588d5c2419570
kleber-matos <Klebermatos2019@icloud.com>
2022-12-07 15:50:37 -0300

    Organizando o codigo

--- assets/script.js
+++ assets/script.js
@@ -1,13 +1,15 @@
+//Variaveis 
 let seach = document.querySelector('#seach');
 let input = document.querySelector('#input');
 let res = document.querySelector('#res')
 let img = document.querySelector('img')
 
+
+//Função do click
 seach.addEventListener('click', () => {
 
     let contryInput = input.value;
     let URL = `https://restcountries.com/v3.1/name/${contryInput}?fullText=true`
-
 
     fetch(URL).then((response)=> response.json())
     .then((data) => { res.innerHTML=`${data[0].capital[0]}`} ).catch(() => alert(`"${input.value}" não encontrado`))   
@@ -24,11 +26,11 @@ input.addEventListener('keyup', (e) => {
         fetch(URL).then((response)=> response.json())
         .then((data) => { 
 
-            console.log(data[0])
-            console.log(data[0].flags.png)
-
-            img.setAttribute('src', data[0].flags.png)
-            res.innerHTML=`${data[0].capital[0]}`}).catch(() => alert(`"${input.value}" não encontrado`))
+        console.log(data[0])
+        console.log(data[0].flags.png)
+        //Print da tela
+        img.setAttribute('src', data[0].flags.png)
+        res.innerHTML=`${data[0].capital[0]}`}).catch(() => alert(`"${input.value}" não encontrado`))
          
     }
 })
